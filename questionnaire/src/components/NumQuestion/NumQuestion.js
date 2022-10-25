2f28271d589f06df88e5f72729534ef8df3f45c6
Camila Horita <camila.horita@solera.com>
2022-10-25 16:32:16 +0200

    requested numericalquestions to the database

--- questionnaire/src/components/NumQuestion/NumQuestion.js
+++ questionnaire/src/components/NumQuestion/NumQuestion.js
@@ -2,11 +2,14 @@ import axios from "axios";
 import "./NumQuestion.css";
 
 function NumQuestion(){
+    function nextQuestion (){
+        return (console.log("next"));
+    }
 
     function getQuestion() {
         axios
       
-        .get("http://localhost:9090/api/boolq", { withCredentials: true })
+        .get("http://localhost:9090/api/numq", { withCredentials: true })
       
         .then(function (response) {
       
@@ -26,17 +29,18 @@ function NumQuestion(){
         <div className="numQuest">
             <p>What do you know about ..... ?</p>
             <div>
-                <div className="radioOption"><label>1<input type="radio" name="value" value="1" checked/></label></div>
+                <div className="radioOption"><label>1<input type="radio" name="value" value="1" /></label></div>
                 <div className="radioOption"><label>2<input type="radio" name="value" value="2" /></label></div>
-                <div className="radioOption"><label>3<input type="radio" name="value" value="3" checked/></label></div>
+                <div className="radioOption"><label>3<input type="radio" name="value" value="3" /></label></div>
                 <div className="radioOption"><label>4<input type="radio" name="value" value="4" /></label></div>
-                <div className="radioOption"><label>5<input type="radio" name="value" value="5" checked/></label></div>
+                <div className="radioOption"><label>5<input type="radio" name="value" value="5" /></label></div>
                 <div className="radioOption"><label>6<input type="radio" name="value" value="6" /></label></div>
-                <div className="radioOption"><label>7<input type="radio" name="value" value="7" checked/></label></div>
+                <div className="radioOption"><label>7<input type="radio" name="value" value="7" /></label></div>
                 <div className="radioOption"><label>8<input type="radio" name="value" value="8" /></label></div>
-                <div className="radioOption"><label>9<input type="radio" name="value" value="9" checked/></label></div>
+                <div className="radioOption"><label>9<input type="radio" name="value" value="9" /></label></div>
                 <div className="radioOption"><label>10<input type="radio" name="value" value="10" /></label></div>      
             </div>
+            <input id="btnquest" type="button" value="NEXT" className="btn btn-primary" onClick={() => nextQuestion()} />
         </div>
     )
 };
